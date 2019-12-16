158399a6ea431b671c4d0b29fd43a217d4724453
unkleandy <ANDREA.MONTREAL.COHEN@GMAIL.COM>
2019-12-16 15:09:19 -0500

    GAParameters est fait

--- EvolutionObserver.h
+++ EvolutionObserver.h
@@ -1,6 +1,7 @@
 #pragma once
 #ifndef _EVOLUTIONOBSERVER_H_
 #define _EVOLUTIONOBSERVER_H_
+#include "GAEngine.h"
 
 
 class EvolutionObserver
@@ -8,6 +9,7 @@ class EvolutionObserver
 public:
 	EvolutionObserver();
 	~EvolutionObserver();
+	virtual void update(GAEngine engine) = 0;
 };
 
 #endif // !_EVOLUTIONOBSERVER_H_
